f6b65add403371e6481aef9fff065cd7a1d72f38
mathetes <mathetes@gmail.com>
2019-02-21 17:06:19 +0700

    Routing done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import AddContact from './components/contacts/AddContact';
 import Contacts from './components/contacts/Contacts';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,13 +13,19 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header />          
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header />          
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />  
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />  
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
